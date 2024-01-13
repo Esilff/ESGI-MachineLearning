25fb92392f8b21f714a6daca296056c3b0d63cd9
Esilff <vbanari02@gmail.com>
2024-01-13 11:40:55 +0100

    adding a script to scrap images from a google images url

---
+++ tools/scrapper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+import urllib
+
+def download_images(url, count):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    images = soup.find_all('img', {'src': re.compile('gstatic.com')})
+
+    if not os.path.exists('downloaded_images'):
+        os.makedirs('downloaded_images')
+    for i, img in enumerate(images[:count]):
+        try:
+            img_url = img['src']
+            img_data = requests.get(img_url).content
+            with open(f'downloaded_images/img_{i+1}.jpg', 'wb') as handler:
+                handler.write(img_data)
+            print(f'Image {i+1} downloaded')
+        except Exception as e:
+            print(f'Error downloading image {i+1}: {e}')
+
+# Example usage
+url = "https://www.google.com/search?client=opera-gx&hs=zN0&sca_esv=598104924&sxsrf=ACQVn095Xhve27bpKDTTgLPvqolK5yQX8A:1705142022370&q=moba&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjlr6OEldqDAxXGVKQEHfQIBlwQ0pQJegQICBAB&biw=1879&bih=977&dpr=1"  # Replace with your Google Images URL
+counter = 5  # Replace with the number of images you want to download
+download_images(url, counter)
