a8d333cba4d4b4a8100c4a0b7a48d6f304e8fa0a
Esilff <vbanari02@gmail.com>
2024-01-14 10:53:06 +0100

    minor update

--- tools/scrapper.py
+++ tools/scrapper.py
@@ -9,8 +9,10 @@ def download_images(url, count):
     soup = BeautifulSoup(response.text, 'html.parser')
     images = soup.find_all('img', {'src': re.compile('gstatic.com')})
 
+    # Create directory for saving images if it does not exist
     if not os.path.exists('downloaded_images'):
         os.makedirs('downloaded_images')
+
     for i, img in enumerate(images[:count]):
         try:
             img_url = img['src']
@@ -23,5 +25,5 @@ def download_images(url, count):
 
 # Example usage
 url = "https://www.google.com/search?client=opera-gx&hs=zN0&sca_esv=598104924&sxsrf=ACQVn095Xhve27bpKDTTgLPvqolK5yQX8A:1705142022370&q=moba&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjlr6OEldqDAxXGVKQEHfQIBlwQ0pQJegQICBAB&biw=1879&bih=977&dpr=1"  # Replace with your Google Images URL
-counter = 5  # Replace with the number of images you want to download
+counter = 1000 # Replace with the number of images you want to download
 download_images(url, counter)
